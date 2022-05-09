30b43bcbc239efdd84bd57f4883a85dce937436e
TRAN Quang Chien <quang-chien.tran@foncia.com>
2022-05-10 00:31:09 +0200

    update tier app

---
+++ multi-tier-application/modules/networking/outputs.tf
@@ -0,0 +1,12 @@
+output "vpc" {
+  value       = module.vpc
+}
+
+output "sg" {
+  value       = {
+      lb = module.lb_sg.security_group.id
+      web = module.web_sg.security_group.id
+      lb = module.db_sg.security_group.id
+  }
+}
+
---
+++ multi-tier-application/modules/networking/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  type        = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+}
+
+variable "private_subnets" {
+  type        = list(string)
+}
+
+variable "database_subnets" {
+  type        = list(string)
+}
